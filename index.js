7ea4915f4dd010d5b5299409a8f11cb6c51bfcc3
Santiago Gil <gil.educaciontdf@gmail.com>
2019-05-06 01:05:25 -0300

    Refactoriza la vista humanos en modulo aparte

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var kappa = require('kappa-core')
-var View = require('kappa-view-level')
-var memdb = require('memdb')
+var humanos = require('./views/humanos')
 
 // TODO reemplazar constantes por mensajes de configuracion + vista de configuracion actual
 const MARGENHUMANO = 20
@@ -10,26 +9,6 @@ const TAZACREDITO = 0
 const PLAZOACREDITACION = 0
 
 var core = kappa('./log', { valueEncoding: 'json' })
-var lvl = memdb()
-
-var humanos = View(lvl, {
-  map: function (msg) {
-    console.log(msg)
-    return [
-      [ msg.value.key, msg.value.value ]  // map first element to second element
-    ]
-  },
-  
-  api: {
-    get: function (core, key, cb) {
-      lvl.get(key, cb)
-    },
-    stream: function (core, opts) {
-      console.log(opts)
-      return lvl.createReadStream(opts)
-    }
-  }
-})
 
 core.use('humanos', humanos)
 
@@ -39,7 +18,6 @@ core.feed('default', function (err, feed) {
   feed.append({key: '0002', value: '{"name": "cacho", "marca": "otra"}'}, console.log)
   feed.append({key: '0001', value: '{"name": "alberto", "marca": "blah"}'}, console.log)
   core.ready('humanos', function () {
-console.log(core.feeds())
     core.api.humanos.stream({ge: '0001', lt: '9999'}).on('data', console.log)
   })
 })
